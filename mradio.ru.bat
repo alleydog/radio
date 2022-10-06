f254354ba9a0b841393d082c1732ed38562cc653
alleydog <51403105+alleydog@users.noreply.github.com>
2022-10-06 08:25:38 +0300

    2022-10-06

--- mradio.ru.bat
+++ mradio.ru.bat
@@ -1,9 +1,13 @@
-@echo off
-curl -sX GET http://mradio.ru/player.html > player.html
-for /f "tokens=2,3" %%I in ('findstr /i "<audio.*></audio>" ^< player.html') do @echo %%J > mradio.tmp
-del -y player.html
-for /f "usebackq  delims=" %%i in (`find /n /v "" mradio.tmp ^| find "[1]"`) do (set value=%%i)
-del -y mradio.tmp
-echo #EXTM3U > mradio.ru.m3u
-echo #EXTINF:-1,mradio.ru >> mradio.ru.m3u
-set /p str3=%value:~7,92% >> mradio.ru.m3u < nul
+@ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
+ECHO #EXTM3U > mradio.ru.m3u
+ECHO #EXTINF:-1,mradio.ru >> mradio.ru.m3u
+CURL -sX GET http://mradio.ru/player.html > player.html
+FOR /F "TOKENS=2,3" %%I IN ('FINDSTR /I "<audio.*></audio>" ^< player.html') DO @ECHO %%J > mradio.tmp
+FOR /F "USEBACKQ DELIMS=" %%I IN (`FIND /N /V "" mradio.tmp ^| FIND "[1]"`) DO (SET value=%%I)
+DEL -Y player.html mradio.tmp
+SET search=_24?
+SET replace=_56?
+SET modified=!value:%search%=%replace%!
+SET /P STR3=%modified:~7,92% >> mradio.ru.m3u < NUL
+ENDLOCAL
--- roks.ru.bat
+++ roks.ru.bat
@@ -1,7 +1,7 @@
-@echo off
-echo #EXTM3U > roks.ru.m3u
-echo #EXTINF:-1,roks.ru >> roks.ru.m3u
-curl -sX GET https://roks.ru/player.php | findstr "https://md5-icecast-radiorocks.cdnvideo.ru/roks.stream?md5=" > roks.tmp
-for /f "usebackq  delims=" %%i in (`find /n /v "" roks.tmp ^| find "[1]"`) do (set value=%%i)
-del -y roks.tmp
-set /p str=%value:~15,100% >> roks.ru.m3u < nul
+@ECHO OFF
+ECHO #EXTM3U > roks.ru.m3u
+ECHO #EXTINF:-1,roks.ru >> roks.ru.m3u
+CURL -sX GET https://roks.ru/player.php | FINDSTR "https://md5-icecast-radiorocks.cdnvideo.ru/roks.stream?md5=" > roks.tmp
+FOR /F "USEBACKQ  DELIMS=" %%I IN (`FIND /N /V "" roks.tmp ^| FIND "[1]"`) DO (SET value=%%I)
+SET /P STR=%value:~15,100% >> roks.ru.m3u < NUL
+DEL -Y roks.tmp
