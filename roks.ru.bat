65ed962453e85db90a5add3dc9fc232f12309dbb
alleydog <51403105+alleydog@users.noreply.github.com>
2023-04-04 20:27:51 +0300

    radio

--- roks.ru.bat
+++ roks.ru.bat
@@ -1,12 +1,12 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-ECHO #EXTM3U> roks.ru.m3u8
-ECHO #EXTINF:-1,Р Р°РґРёРѕ Р РѕРєСЃ>> roks.ru.m3u8
+ECHO #EXTM3U> roks.ru.m3u
+ECHO #EXTINF:-1,Радио Рокс>> roks.ru.m3u
 CURL -sX GET https://roks.ru/player.php | FINDSTR "https://md5-icecast-radiorocks.cdnvideo.ru/roks.stream?md5=" > roks.tmp
 FOR /F "USEBACKQ  DELIMS=" %%I IN (`FIND /N /V "" roks.tmp ^| FIND "[1]"`) DO (SET value=%%I)
 DEL -Y roks.tmp
 SET search=https
 SET replace=http
 SET modified=!value:%search%=%replace%!
-SET /P STR=%modified:~15,100% >> roks.ru.m3u8 < NUL
+SET /P STR=%modified:~15,100% >> roks.ru.m3u < NUL
 ENDLOCAL
