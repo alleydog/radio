419f7259257bfe5e857baf60fef2c85c770a4b5d
alleydog <51403105+alleydog@users.noreply.github.com>
2023-07-23 15:46:11 +0300

    radio

--- mradio.ru.bat
+++ mradio.ru.bat
@@ -4,10 +4,13 @@ ECHO #EXTM3U> mradio.ru.m3u
 ECHO #EXTINF:-1,mradio.ru>> mradio.ru.m3u
 CURL -sX GET http://mradio.ru/player.html > player.html
 FOR /F "TOKENS=2,3" %%I IN ('FINDSTR /I "<audio.*></audio>" ^< player.html') DO @ECHO %%J > mradio.tmp
-FOR /F "USEBACKQ DELIMS=" %%I IN (`FIND /N /V "" mradio.tmp ^| FIND "[1]"`) DO (SET value=%%I)
+FOR /F "USEBACKQ DELIMS=" %%I IN (`FIND /N /V "" mradio.tmp ^| FIND "[1]"`) DO (SET source=%%I)
+SET /P STR3=%source:~7,92% >> mradio.ru.m3u < NUL
+ECHO.>> mradio.ru.m3u
+ECHO #EXTINF:-1,mradio.ru>> mradio.ru.m3u
 SET search=_24
 SET replace=_56
-SET modified=!value:%search%=%replace%!
-SET /P STR3=%modified:~7,92% >> mradio.ru.m3u < NUL
+SET value=!source:%search%=%replace%!
+SET /P STR3=%value:~7,92% >> mradio.ru.m3u < NUL
 DEL -Y player.html mradio.tmp
 ENDLOCAL
