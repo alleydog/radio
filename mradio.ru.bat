58c9f523d63be95e5f56803093852d6c17802967
alleydog <51403105+alleydog@users.noreply.github.com>
2024-01-03 12:23:53 +0300

    radio

--- mradio.ru.bat
+++ mradio.ru.bat
@@ -5,12 +5,13 @@ ECHO #EXTINF:-1,M-Radio>> mradio.ru.m3u
 CURL -sX GET http://mradio.ru/player.html > player.html
 FOR /F "TOKENS=2,3" %%I IN ('FINDSTR /I "<audio.*></audio>" ^< player.html') DO @ECHO %%J > mradio.tmp
 FOR /F "USEBACKQ DELIMS=" %%I IN (`FIND /N /V "" mradio.tmp ^| FIND "[1]"`) DO (SET source=%%I)
-SET /P STR3=%source:~7,92% >> mradio.ru.m3u < NUL
-ECHO.>> mradio.ru.m3u
-ECHO #EXTINF:-1,M-Radio>> mradio.ru.m3u
 SET search=_24
 SET replace=_56
 SET value=!source:%search%=%replace%!
 SET /P STR3=%value:~7,92% >> mradio.ru.m3u < NUL
 DEL -Y player.html mradio.tmp
+ECHO.>> mradio.ru.m3u
+ECHO #EXTINF:-1,M-Radio>> mradio.ru.m3u
+SET /P STR3=%source:~7,92% >> mradio.ru.m3u < NUL
 ENDLOCAL
+START vlc mradio.ru.m3u
