11b8af9cfae985fc4e925206feb1bb688737a061
alleydog <51403105+alleydog@users.noreply.github.com>
2025-11-05 13:58:10 +0300

    radio

---
+++ mradio.ru.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "#EXTM3U" > mradio.ru.m3u
+echo "#EXTINF:-1,mradio.ru, 28 kbps" >> mradio.ru.m3u
+curl -sX GET http://mradio.ru/player.html | grep -F '&' | grep 'kbps28' | sed -e 's/<input id="kbps28" type="radio" name="kbps" checked="checked" value="//g'  | sed -e 's/">//g' | sed -e 's/ //g' >> mradio.ru.m3u
+echo "#EXTINF:-1,mradio.ru, 64 kbps" >> mradio.ru.m3u
+curl -sX GET http://mradio.ru/player.html | grep -F '&' | grep 'kbps64' | sed -e 's/<input id="kbps64" type="radio" name="kbps" value="//g' | sed -e 's/">//g' | sed -e 's/ //g' >> mradio.ru.m3u
